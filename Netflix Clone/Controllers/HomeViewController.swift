1a1ac51521a30f301d1e5eece8e900c006ca58a6
ademkoyan <ademkoyan@gmail.com>
2023-02-13 22:13:33 +0300

    fixed navBar bugs and adding when page scrolling being hidden

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -16,6 +16,8 @@ class HomeViewController: UIViewController {
         return table
     } ()
     
+    let sectionTitles: [String] = ["Trending Movies", "Populer", "Trending Tv", "Upcoming Movies","Top Rated"]
+    
     //MARK: -LC
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,14 +38,26 @@ class HomeViewController: UIViewController {
     }
     
     private func configureNavBar() {
-        var image = UIImage(named: "netflix-logo")
-        image = image?.withRenderingMode(.alwaysOriginal)
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 0, height: 34)
+        button.setImage(UIImage(named: "netflix-logo"), for: .normal)
+        //can add a target here for button
+        
+        //Adding to Constraite(width-height) for leftBarButton using custom image
+        let leftButton = UIBarButtonItem(customView: button)
+        let currWidth = leftButton.customView?.widthAnchor.constraint(equalToConstant: 44)
+        currWidth?.isActive = true
+        let currHeight = leftButton.customView?.heightAnchor.constraint(equalToConstant: 34)
+        currHeight?.isActive = true
+        self.navigationItem.leftBarButtonItem = leftButton
+        
+        navigationItem.title = "Netflix"
         
         navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(image: image, style: .done, target: self, action: nil),
             UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
             UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
         ]
+        
     }
 }
     
@@ -52,7 +66,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     //Tablonun kendini tekrarlama sayısı
     func numberOfSections(in tableView: UITableView) -> Int {
-        20
+        return sectionTitles.count
     }
     //Tablonun eleman\satır sayısı
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,5 +87,24 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOfSet = view.safeAreaInsets.top
+        let offSet = scrollView.contentOffset.y + defaultOfSet
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offSet))
     }
 }
