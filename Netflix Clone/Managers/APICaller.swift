211fc2d6115b6c78758f0e86fd1bf71aee433923
ademkoyan <ademkoyan@gmail.com>
2023-02-15 22:40:14 +0300

    networkLayer-2

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -13,21 +13,26 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedTogetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completien: @escaping (String) -> Void) {
+    func getTrendingMovies(completien: @escaping (Result<[Movie], Error>) -> Void) {
         
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
                 return
             }
+            
             do {
-                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-                print(results)
+                let results = try JSONDecoder().decode(TrendingMovieResponse.self, from: data)
+                completien(.success(results.results))
             } catch {
-                print(error.localizedDescription)
+                completien(.failure(error))
             }
         }
         
